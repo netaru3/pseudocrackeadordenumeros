feddec8878e24ffd67f2efa6d4e5c892c233359f
Aarón <netaru3@gmail.com>
2025-08-08 23:03:41 -0300

    Add files via upload

---
+++ 06-creakeador.js
@@ -0,0 +1,9 @@
+function crakeador(){let contraseña=Number(document.getElementById("numero").value); let fecha1=new Date()
+let i=0
+let id=setInterval(() => {
+if(contraseña!=i){i++; document.getElementById("crakeo").textContent=i}
+else{document.getElementById("crakeo").textContent='su contraseña es:'+i;let fecha2=new Date();
+    let tiempototal=fecha2-fecha1
+document.getElementById("tiempo").textContent="el tiempo que demoró crakearlo fue:" +tiempototal +"ms "+"o "+(fecha2-fecha1)/1000+"sg"
+clearInterval(id);return i}; 
+}, 1);}
