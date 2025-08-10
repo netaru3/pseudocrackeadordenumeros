b018802e5f9e0b68bd3b8af3288c44abd21bbe67
Aarón <netaru3@gmail.com>
2025-08-10 19:33:57 -0300

    Update 06-creakeador.js

--- 06-creakeador.js
+++ 06-creakeador.js
@@ -1,8 +1,8 @@
 function crakeador(){let contraseña=Number(document.getElementById("numero").value); let fecha1=new Date()
 let i=0
 let id=setInterval(() => {
-if(contraseña!=i){i++; document.getElementById("crakeo").textContent=i}
-else{document.getElementById("crakeo").textContent='su contraseña es:'+i;let fecha2=new Date();
+if(contraseña!=i && contraseña>=0){i++; document.getElementById("crakeo").textContent=i}
+else if(contraseña>=0){document.getElementById("crakeo").textContent='su contraseña es:'+i;let fecha2=new Date();
     let tiempototal=fecha2-fecha1
 document.getElementById("tiempo").textContent="el tiempo que demoró crakearlo fue:" +tiempototal +"ms "+"o "+(fecha2-fecha1)/1000+"sg"
 clearInterval(id);return i}; 
